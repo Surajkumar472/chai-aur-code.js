41838a336a84dadce9d9b7382beb83a5097a533a
Surajkumar472 <159039148+Surajkumar472@users.noreply.github.com>
2024-03-16 10:32:13 +0000

    conversionconfig

---
+++ 01_basics/02_conversionoperation.js
@@ -0,0 +1,19 @@
+let score = `null`
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let isLoggedin = true
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
