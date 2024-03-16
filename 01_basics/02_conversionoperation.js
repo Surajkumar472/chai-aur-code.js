e06a79af37de610412c62df52c50e58134fd5b52
Surajkumar472 <159039148+Surajkumar472@users.noreply.github.com>
2024-03-16 11:35:27 +0000

    operation

--- 01_basics/02_conversionoperation.js
+++ 01_basics/02_conversionoperation.js
@@ -17,3 +17,25 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+/*  operations */
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+
+let str1 = "hello"
+let str2 = " suraj"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
